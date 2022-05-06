df521ef278048193dddbf68c9c4bfce27ee12a4a
simplellr <102459506+simplellr@users.noreply.github.com>
2022-05-06 08:20:29 +0530

    Add files via upload

---
+++ Footballcontext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication7.Models
+{
+    public class FootBallContext : DbContext
+    {
+        public FootBallContext(): base("name = FTDB")
+        {
+
+        }
+        public DbSet<Football> FootBallLeagueTable { get; set; }
+    }
+}
